246f2af0ea8c269c3578712cfa2e7371ced59ed5
Italo Nunes Leite <italonleite@gmail.com>
2020-08-04 23:42:39 -0300

    - adicionando EntityFrameworkCore - usando databaseInMemory - criando dbContext

--- src/EcommerceApi/Startup.cs
+++ src/EcommerceApi/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EcommerceApi.Infra;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +27,8 @@ namespace EcommerceApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //adicionando banco de dados em memória
+            services.AddDbContext<EcommerceDbContext>(options => options.UseInMemoryDatabase("EcommerceDb"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
